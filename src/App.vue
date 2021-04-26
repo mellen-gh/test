6f914fd16a77c6514e3173f4024f004285961bb1
Vlad_Work <v.volosin@itmo.ru>
2021-04-26 18:59:07 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,43 @@
 <template>
-  <div class="row">
+  <div class="row hello">
     <div class="col-2">
       <button class="btn btn-secondary button" @click="sort">
         Как было
       </button>
     </div>
 
-    <div class="col-6">
-      <h3>TEST DRAG MAF</h3>
-      <draggable
-          class="list-group"
-          tag="transition-group"
-          :component-data="{
+
+    <h3>TEST DRAG MAF</h3>
+    <draggable
+        class="list-group"
+
+        tag="transition-group"
+        :component-data="{
           tag: 'ul',
-          type: 'transition-group',
-          name: !drag ? 'flip-list' : null
+          type: 'transition',
+          name: 'flip-list'
         }"
-          v-model="list"
-          v-bind="dragOptions"
-          @start="drag = true"
-          @end="endDrag"
-          item-key="id"
-      >
-        <template #item="{ element }">
-          <li class="list-group-item">
+        v-model="list"
+        v-bind="dragOptions"
+        @start="drag = true"
+        @end="endDrag"
+        item-key="id"
+    >
 
-            {{element.id}}  {{ element.name }}
-          </li>
-        </template>
-      </draggable>
-    </div>
+      <template #item="{ element }">
+        <li class="list-group-item">
 
-<!--    <rawDisplayer class="col-3" :value="list" title="List"/>-->
-<!--    &ndash;&gt;-->
+          <strong>{{ element.name }}</strong>  <span>{{ element.id }} </span>
+        </li>
+      </template>
+
+    </draggable>
+    <p><strong>Прошлый:</strong>{{ oldIndex }}</p>
+    <p><strong>Новый:</strong> {{ newIndex }}</p>
+
+
+    <!--    <rawDisplayer class="col-3" :value="list" title="List"/>-->
+    <!--    &ndash;&gt;-->
   </div>
 </template>
 
@@ -57,18 +62,22 @@ export default {
         {name: "Женя", id: 5},
 
       ],
-      drag: false
+      drag: false,
+      oldIndex: '',
+      newIndex: ''
     };
   },
   methods: {
-    endDrag() {
+    endDrag(evt) {
       this.drag = false;
-      console.log(this.list)
+      console.log(evt)
+      this.oldIndex = evt.oldIndex;
+      this.newIndex = evt.newIndex;
     },
     sort() {
-       this.list = this.list.sort((a, b) => a.id - b.id);
+      this.list = this.list.sort((a, b) => a.id - b.id);
 
-     }
+    }
   },
   computed: {
     dragOptions() {
@@ -83,7 +92,31 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+strong {
+  display: inline-block;
+}
+
+.sortable {
+
+}
+
+.list-group {
+  padding-inline-start: 0px;
+}
+
+#app {
+  padding: 4em;
+}
+
+body {
+  background-color: rgb(231, 231, 231);
+}
+
+#app {
+  background-color: rgb(231, 231, 231);
+}
+
 .button {
   margin-top: 35px;
 }
@@ -106,13 +139,49 @@ export default {
 }
 
 .list-group-item {
-  padding: 8px;
-  border: 1px solid black;
-  margin: 2px;
+
   cursor: move;
 }
 
+
+
 .list-group-item i {
   cursor: pointer;
+}
+
+.sortable-chosen {
+  opacity: 0;
+}
+
+.list-group-item {
+  width: 100%;
+  background-color: #fff;
+  padding: 1em;
+  list-style-type: none;
+  margin-bottom: 2px;
+  span {
+    float: right;
+  }
+}
+.sortable-chosen {
+  opacity: 0.4;
+}
+.sortable-chosen {
+  transition: transform 0.5s;
+}
+.ghost {
+  border-left: 6px solid #42b983;
+  box-shadow: 10px 10px 5px -1px rgba(0,0,0,0.14);
+  opacity: .7;
+
+  &::before{
+    content: " ";
+
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    margin-left: -50px;
+    background-image: url("../public/123.svg") ;
+  }
 }
 </style>
