6a35cd6f3a6933da3ae1b48c8b9695629c6d0457
Vlad_Work <v.volosin@itmo.ru>
2021-04-26 15:36:25 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,118 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col-2">
+      <button class="btn btn-secondary button" @click="sort">
+        Как было
+      </button>
+    </div>
+
+    <div class="col-6">
+      <h3>TEST DRAG MAF</h3>
+      <draggable
+          class="list-group"
+          tag="transition-group"
+          :component-data="{
+          tag: 'ul',
+          type: 'transition-group',
+          name: !drag ? 'flip-list' : null
+        }"
+          v-model="list"
+          v-bind="dragOptions"
+          @start="drag = true"
+          @end="endDrag"
+          item-key="id"
+      >
+        <template #item="{ element }">
+          <li class="list-group-item">
+
+            {{element.id}}  {{ element.name }}
+          </li>
+        </template>
+      </draggable>
+    </div>
+
+<!--    <rawDisplayer class="col-3" :value="list" title="List"/>-->
+<!--    &ndash;&gt;-->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import draggable from "vuedraggable";
 
 export default {
-  name: 'App',
+  name: "transition-example-2",
+  display: "Transitions",
+  order: 0,
   components: {
-    HelloWorld
+    draggable
+  },
+  data() {
+    return {
+      list: [
+        {name: "Артём", id: 0},
+        {name: "Влад", id: 1},
+        {name: "Маша", id: 2},
+        {name: "Вова", id: 3},
+        {name: "Ира", id: 4},
+        {name: "Женя", id: 5},
+
+      ],
+      drag: false
+    };
+  },
+  methods: {
+    endDrag() {
+      this.drag = false;
+      console.log(this.list)
+    },
+    sort() {
+       this.list = this.list.sort((a, b) => a.id - b.id);
+
+     }
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost"
+      };
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.button {
+  margin-top: 35px;
+}
+
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.no-move {
+  transition: transform 0s;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.list-group {
+  min-height: 20px;
+}
+
+.list-group-item {
+  padding: 8px;
+  border: 1px solid black;
+  margin: 2px;
+  cursor: move;
+}
+
+.list-group-item i {
+  cursor: pointer;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,62 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<draggable v-model="myArray" tag="transition-group" ghost-class="ghost" @start="drag=true" @end="drag-false"
+           item-key="id">
+<template #item="{element}">
+  <div>{{ element.name }}</div>
 </template>
+</draggable>
 
 <script>
+import draggable from 'vuedraggable'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      myArray: [
+        {name: "Angular", id: 0},
+        {name: "React", id: 1},
+        {name: "Vue", id: 2},
+        {name: "HTML", id: 3},
+        {name: "CSS", id: 4},
+        {name: "Sass", id: 5},
+
+      ],
+      oldIndex: '',
+      newIndex: ''
+    }
+  },
+  methods: {
+    onEnd(evt) {
+      console.log(evt)
+      this.oldIndex = evt.oldIndex;
+      this.newIndex = evt.newIndex;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
